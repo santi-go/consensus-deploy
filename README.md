25c3d0716bae54d86b6aa97bcd62088a9b43c233
Raül Martínez i Peris <raul@cuchame.es>
2018-03-13 15:56:24 +0100

    Readme

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 ## Prepare project
 
-Copy netrc in project folder.
+Copy '~/.netrc' in project folder as 'netrc'.
 
 
 ## Build application
 
 ~~~
-docker build .
+docker build --rm --no-cache -t deploy_heroku .
+~~~
+
+
+## Delete build
+
+~~~
+docker rmi deploy_heroku
 ~~~
--- deploy_in_heroku.sh
+++ deploy_in_heroku.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-COMMIT='New deploy'
-
+echo "\nConfigure git user..."
 git config --global user.email 'devscolavlc@gmail.com'
 git config --global user.name 'Devscola Run2017-2018'
 
+echo "\nPrepare heroku login..."
 mv netrc .netrc
+
+echo "\nCloning Heroku repository..."
+heroku git:clone -a consensus17
 
 echo "\nCloning Consensus repository..."
 git clone https://gitlab.com/devscola/consensus.git
+cd consensus
+APP=$( git log --format='%s from %cn' -1 )
+cd ..
+echo "cloned $APP"
 
 echo "\nBuilding Consensus project..."
 cd consensus
@@ -15,14 +22,15 @@ npm install
 npm run build-deploy
 cd ..
 
-echo "\nCloning Consensus-api repository..."
-git clone https://gitlab.com/devscola/consensus-api.git
-
-echo "Cloning Heroku repository..."
-heroku git:clone -a consensus17
-
 echo "\nCopying consensus app to Heroku files..."
 cp -r consensus/public consensus17/
+
+echo "\nCloning Consensus-api repository..."
+git clone https://gitlab.com/devscola/consensus-api.git
+cd consensus-api
+API=$( git log --format='%s from %cn' -1 )
+cd ..
+echo "cloned $API"
 
 echo "\nCopying files from consensus-api to Heroku files..."
 cp consensus-api/app.rb consensus17/
@@ -37,7 +45,7 @@ cp -r consensus-api/templates consensus17/
 echo "\nCommitting changes to Heroku repository..."
 cd consensus17
 git add .
-git commit -m "$COMMIT"
+git commit -m "Deploy from: $API and $APP"
 git push heroku master
 cd ..
 
